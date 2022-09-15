2bff387b10a0f49e55890b27c8fbe0ba1d6987a5
mrtmeeseeks <tao.disha@gmail.com>
2022-09-15 19:59:31 +0300

    fixed cache issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MainWrapper from "./_main_wrapper";
 import Portal from "containers/OptOut/Portal";
 import ExpanderSection from "containers/OptOut/ExpanderSection";
 import VideoSection from "containers/VideoSection";
 import OptOutTop from "containers/OptOut/OptOutTop";
 import { OptOutData } from "common/data";
+import { Init } from "@aut-protocol/d-aut";
 
 const Main = () => {
+  useEffect(() => {
+    console.log("document: ", document);
+    Init();
+  }, []);
   return (
     <MainWrapper>
+      <d-aut
+        id="sw-auth"
+        network="mumbai"
+        button-type="simple"
+        dao-expander="0x6bBE078Be00Ca7D59dab4eA1a297514497f86ab8"
+      />
+
       <OptOutTop />
       <Portal />
       <ExpanderSection />
