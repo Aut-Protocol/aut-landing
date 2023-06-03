4c49910d331c798a6f80e8d6472ac4a48a45f361
Iulia <52575560+iuliagabriela@users.noreply.github.com>
2023-06-03 23:37:22 +0300

    font license

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -44,6 +44,22 @@ export default class CustomDocument extends Document {
           />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          {/*           
+            /**
+            * @license
+            * MyFonts Webfont Build ID 954498
+            *
+            * The fonts listed in this notice are subject to the End User License
+            * Agreement(s) entered into by the website owner. All other parties are
+            * explicitly restricted from using the Licensed Webfonts(s).
+            *
+            * You may obtain a valid license from one of MyFonts official sites.
+            * http://www.fonts.com
+            * http://www.myfonts.com
+            * http://www.linotype.com
+            *
+            */}
+
           <style>{LoaderStyles}</style>
         </Head>
         <body>
