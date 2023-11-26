f1e1c0074630330c939104d9402be78d28b22c7f
mrtmeeseeks <tao.disha@gmail.com>
2023-11-26 23:54:17 +0200

    Added main wrapper

---
+++ src/pages/_main_wrapper.js
@@ -0,0 +1,25 @@
+import { ContentWrapper } from "containers/app.style";
+
+import Footer from "containers/Footer";
+import React from "react";
+import "react-perfect-scrollbar/dist/css/styles.css";
+import PerfectScrollbar from "react-perfect-scrollbar";
+
+const MainWrapper = ({ children }) => {
+  return (
+    <PerfectScrollbar
+      style={{
+        height: "100vh",
+      }}
+      options={{
+        suppressScrollX: true,
+        useBothWheelAxes: false,
+      }}
+    >
+      {children}
+      {/* <Footer /> */}
+    </PerfectScrollbar>
+  );
+};
+
+export default MainWrapper;
